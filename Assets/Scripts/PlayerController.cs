c41e37ef48de48ec64b747877653e14af7672e04
naaainuuu <chohanhasnain9@gmail.com>
2023-11-28 19:11:54 +0500

    Movement and Sprinting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,10 @@ public class PlayerController : MonoBehaviour
     public Transform viewPoint;
     public float mouseSensivity = 1f;
     public bool invertLook;
-    public float moveSpeed = 5f;
+    public float moveSpeed = 5f, runSpeed = 8f;
+    public float activeMoveSpeed;
+
+    public CharacterController charCon;
     
 
     private float verticalRotStore;
@@ -26,16 +29,25 @@ public class PlayerController : MonoBehaviour
         verticalRotStore = Mathf.Clamp(verticalRotStore, -90f, 90f);
         if(invertLook)
         {
-            viewPoint.rotation = Quaternion.Euler(viewPoint.rotation.eulerAngles.x, viewPoint.rotation.eulerAngles.y, verticalRotStore);
+            viewPoint.rotation = Quaternion.Euler(verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
         }
         else
         {
-            viewPoint.rotation = Quaternion.Euler(viewPoint.rotation.eulerAngles.x, viewPoint.rotation.eulerAngles.y, -verticalRotStore);
+            viewPoint.rotation = Quaternion.Euler(-verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
         }
 
-        movement = ((transform.forward * moveDir.z) + (transform.right * moveDir.x)).normalized;
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            activeMoveSpeed = runSpeed;
+        }
+        else
+        {
+            activeMoveSpeed = moveSpeed;
+        }
 
         moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        movement = ((transform.forward * moveDir.z) + (transform.right * moveDir.x)).normalized * activeMoveSpeed;
+        charCon.Move(movement * Time.deltaTime);
     }
 }
