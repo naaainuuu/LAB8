e315e16fdc58cf833daf248b5a4158ce56b64443
naaainuuu <chohanhasnain9@gmail.com>
2023-11-28 23:46:41 +0500

    Shooting and Automatic Shooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,12 +24,17 @@ public class PlayerController : MonoBehaviour
     private bool isGrounded;
     public LayerMask groundlayers;
 
+    public GameObject bulletImpact;
+    public float timeBetweenShots = 0.1f;
+    private float shotCounter;
+
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         cam = Camera.main;
     }
-    // Update is called once per frame
+
     void Update()
     {
         mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensivity;
@@ -73,7 +78,47 @@ public class PlayerController : MonoBehaviour
 
         movement.y += Physics.gravity.y * Time.deltaTime * gravityMod;
         charCon.Move(movement * Time.deltaTime);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+        if(Input.GetMouseButton(0))
+        {
+            shotCounter -= Time.deltaTime;
+            if(shotCounter <= 0) 
+            {
+                Shoot();
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else if(Cursor.lockState == CursorLockMode.None)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+
     }
+
+    private void Shoot()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(.5f, .5f, 0));
+        ray.origin = cam.transform.position;
+        if(Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Debug.Log("We hit " + hit.collider.gameObject.name);
+            GameObject bulletImpactObject = Instantiate(bulletImpact, hit.point + (hit.normal * 0.002f), Quaternion.LookRotation(hit.normal, Vector3.up));
+            Destroy(bulletImpactObject, 5f);
+        }
+        shotCounter = timeBetweenShots;
+    }
+
     void LateUpdate()
     {
         cam.transform.position = viewPoint.position;
