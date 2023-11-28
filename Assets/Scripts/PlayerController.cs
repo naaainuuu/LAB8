db0bb1b6ad1ab7368b9bcbebc8d6e198b6dd56bb
naaainuuu <chohanhasnain9@gmail.com>
2023-11-29 00:11:50 +0500

    Overheating

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,8 +25,12 @@ public class PlayerController : MonoBehaviour
     public LayerMask groundlayers;
 
     public GameObject bulletImpact;
-    public float timeBetweenShots = 0.1f;
+    public float timeBetweenShots = 0.5f;
     private float shotCounter;
+
+    public float maxHeat = 10f, heatPerShot = .1f, coolRate = 4f, overheatCoolRate = 5f;
+    private float heatCounter;
+    private bool overHeated;
 
 
     void Start()
@@ -37,10 +41,12 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        //Viewing Around using mouse
         mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensivity;
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x,transform.rotation.eulerAngles.z);
         verticalRotStore += mouseInput.y;
         verticalRotStore = Mathf.Clamp(verticalRotStore, -90f, 90f);
+        //Inverting Mouse look
         if(invertLook)
         {
             viewPoint.rotation = Quaternion.Euler(verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
@@ -50,7 +56,10 @@ public class PlayerController : MonoBehaviour
             viewPoint.rotation = Quaternion.Euler(-verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
         }
 
+        //movement
         moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+
+        //Sprinting
         if (Input.GetKey(KeyCode.LeftShift))
         {
             activeMoveSpeed = runSpeed;
@@ -64,6 +73,7 @@ public class PlayerController : MonoBehaviour
         movement = ((transform.forward * moveDir.z) + (transform.right * moveDir.x)).normalized * activeMoveSpeed;
         movement.y = yVel;
 
+        //checking grounded
         if(charCon.isGrounded)
         {
             movement.y = 0f;
@@ -71,27 +81,47 @@ public class PlayerController : MonoBehaviour
 
         isGrounded = Physics.Raycast(groundCheckPoint.position, Vector3.down, .25f, groundlayers);
 
+        //Jumping
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
             movement.y = jumpForce;
         }
 
+        //gravity
         movement.y += Physics.gravity.y * Time.deltaTime * gravityMod;
         charCon.Move(movement * Time.deltaTime);
 
-        if(Input.GetMouseButtonDown(0))
+        //shooting with overheating
+        if(!overHeated)
         {
-            Shoot();
-        }
-        if(Input.GetMouseButton(0))
-        {
-            shotCounter -= Time.deltaTime;
-            if(shotCounter <= 0) 
+            if (Input.GetMouseButtonDown(0))
             {
                 Shoot();
             }
+            if (Input.GetMouseButton(0))
+            {
+                shotCounter -= Time.deltaTime;
+                if (shotCounter <= 0)
+                {
+                    Shoot();
+                }
+            }
+            heatCounter -= coolRate * Time.deltaTime;
         }
-
+        else
+        {
+            heatCounter -= overheatCoolRate * Time.deltaTime;
+            if (heatCounter <= 0)
+            {
+                overHeated = false;
+            }
+        }
+        if (heatCounter < 0)
+        {
+            heatCounter = 0;
+        }
+        
+        //unlcoking cursor in build
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
@@ -108,6 +138,7 @@ public class PlayerController : MonoBehaviour
 
     private void Shoot()
     {
+        //loacting where the shot is hit using raycast
         Ray ray = cam.ViewportPointToRay(new Vector3(.5f, .5f, 0));
         ray.origin = cam.transform.position;
         if(Physics.Raycast(ray, out RaycastHit hit))
@@ -116,11 +147,22 @@ public class PlayerController : MonoBehaviour
             GameObject bulletImpactObject = Instantiate(bulletImpact, hit.point + (hit.normal * 0.002f), Quaternion.LookRotation(hit.normal, Vector3.up));
             Destroy(bulletImpactObject, 5f);
         }
+
+        //automatic shooting
         shotCounter = timeBetweenShots;
+
+        //overheating
+        heatCounter += heatPerShot;
+        if(heatCounter >= maxHeat)
+        {
+            heatCounter = maxHeat;
+            overHeated = true;
+        }
     }
 
     void LateUpdate()
     {
+        //moving camera to the correct position
         cam.transform.position = viewPoint.position;
         cam.transform.rotation = viewPoint.rotation;
     }
