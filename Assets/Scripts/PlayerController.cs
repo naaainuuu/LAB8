c8eacab511cbf5ec053bc8fbc33a5a91f77a9ab9
naaainuuu <chohanhasnain9@gmail.com>
2023-11-28 19:38:28 +0500

    Gravity and Jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public bool invertLook;
     public float moveSpeed = 5f, runSpeed = 8f;
     public float activeMoveSpeed;
+    public float jumpForce = 12f, gravityMod = 2.5f;
+
 
     public CharacterController charCon;
     
@@ -16,9 +18,13 @@ public class PlayerController : MonoBehaviour
     private Vector2 mouseInput;
     private Vector3 moveDir, movement;
 
+
+    private Camera cam;
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        cam = Camera.main;
     }
     // Update is called once per frame
     void Update()
@@ -36,8 +42,8 @@ public class PlayerController : MonoBehaviour
             viewPoint.rotation = Quaternion.Euler(-verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
         }
 
-
-        if(Input.GetKey(KeyCode.LeftShift))
+        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             activeMoveSpeed = runSpeed;
         }
@@ -46,8 +52,26 @@ public class PlayerController : MonoBehaviour
             activeMoveSpeed = moveSpeed;
         }
 
-        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        float yVel = movement.y;
         movement = ((transform.forward * moveDir.z) + (transform.right * moveDir.x)).normalized * activeMoveSpeed;
+        movement.y = yVel;
+
+        if(charCon.isGrounded)
+        {
+            movement.y = 0f;
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            movement.y = jumpForce;
+        }
+
+        movement.y += Physics.gravity.y * Time.deltaTime * gravityMod;
         charCon.Move(movement * Time.deltaTime);
+    }
+    void LateUpdate()
+    {
+        cam.transform.position = viewPoint.position;
+        cam.transform.rotation = viewPoint.rotation;
     }
 }
