06d6886d9608e222474e4d593c78095260dea3a7
naaainuuu <chohanhasnain9@gmail.com>
2023-11-29 20:32:25 +0500

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -164,6 +164,15 @@ public class PlayerController : MonoBehaviour
             }
             SwitchGun();
         }
+
+        for(int i = 0; i < allGuns.Length; i++)
+        {
+            if(Input.GetKeyDown((i+1).ToString()))
+            {
+                selectedGun = i;
+                SwitchGun();
+            }
+        }
         
         //unlcoking cursor in build
         if(Input.GetKeyDown(KeyCode.Escape))
